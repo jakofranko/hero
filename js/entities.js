2fa3ae37927584863eac7df4f7b8af8c4d6ed58e
Jake Franklin <jfranklin@telesphere.com>
2015-11-21 00:25:31 -0700

    Finished up part 6

--- js/entities.js
+++ js/entities.js
@@ -6,9 +6,13 @@ Game.Mixins.Moveable = {
 	tryMove: function(x, y, map) {
 		var tile = map.getTile(x, y);
 		var target = map.getEntityAt(x, y);
-		console.log(target);
 		if(target) {
-			return false;
+			if(this.hasMixin('Attacker')) {
+				this.attack(target);
+				return true;
+			} else {
+				return false;	
+			}
 		} else if(tile.isWalkable()) {
 			this._x = x;
 			this._y = y;
@@ -20,6 +24,30 @@ Game.Mixins.Moveable = {
 		return false;
 	}
 };
+
+Game.Mixins.Destructible = {
+	name: 'Destructible',
+	init: function() {
+		this._hp = 1;
+	},
+	takeDamage: function(attack, damage) {
+		this._hp -= damage;
+		if(this._hp <= 0) {
+			this.getMap().removeEntity(this);
+		}
+	}
+};
+
+Game.Mixins.SimpleAttacker = {
+    name: 'SimpleAttacker',
+    groupName: 'Attacker',
+    attack: function(target) {
+        // Only remove the entity if they were attackable
+        if (target.hasMixin('Destructible')) {
+            target.takeDamage(this, 1);
+        }
+    }
+}
 
 Game.Mixins.PlayerActor = {
 	name: 'PlayerActor',
@@ -35,18 +63,44 @@ Game.Mixins.PlayerActor = {
 Game.Mixins.FungusActor = {
 	name: 'FungusActor',
 	groupName: 'Actor',
-	act: function() {}
+	init: function() {
+        this._growthsRemaining = 5;
+    },
+	act: function() {
+		if(this._growthsRemaining > 0) {
+			if(Math.random() <= 0.02) {
+				// Generate the coordinates of a random adjacent square by
+                // generating an offset between [-1, 0, 1] for both the x and
+                // y directions. To do this, we generate a number from 0-2 and then
+                // subtract 1.
+                var xOffset = Math.floor(Math.random() * 3) - 1;
+                var yOffset = Math.floor(Math.random() * 3) - 1;
+                // Make sure we aren't trying to spawn on the same tile as us
+                if (xOffset != 0 || yOffset != 0) {
+                    // Check if we can actually spawn at that location, and if so
+                    // then we grow!
+                    if (this.getMap().isEmptyFloor(this.getX() + xOffset, this.getY() + yOffset)) {
+                        var entity = new Game.Entity(Game.FungusTemplate);
+                        entity.setX(this.getX() + xOffset);
+                        entity.setY(this.getY() + yOffset);
+                        this.getMap().addEntity(entity);
+                        this._growthsRemaining--;
+                    }
+                }
+			}
+		}
+	}
 }
 
 Game.PlayerTemplate = {
 	character: '@',
 	foreground: 'white',
 	background: 'black',
-	mixins: [Game.Mixins.Moveable, Game.Mixins.PlayerActor]
+	mixins: [Game.Mixins.Moveable, Game.Mixins.PlayerActor, Game.Mixins.Destructible, Game.Mixins.SimpleAttacker]
 };
 
 Game.FungusTemplate = {
 	character: 'F',
 	foreground: 'green',
-	mixins: [Game.Mixins.FungusActor]
+	mixins: [Game.Mixins.FungusActor, Game.Mixins.Destructible]
 };
--- js/map.js
+++ js/map.js
@@ -15,7 +15,7 @@ Game.Map = function(tiles, player) {
     // add the player
     this.addEntityAtRandomPosition(player);
     // add random fungi
-    for (var i = 0; i < 1000; i++) {
+    for (var i = 0; i < 50; i++) {
         this.addEntityAtRandomPosition(new Game.Entity(Game.FungusTemplate));
     }
 };
@@ -69,6 +69,19 @@ Game.Map.prototype.addEntity = function(entity) {
 		this._scheduler.add(entity, true);
 	}
 };
+Game.Map.prototype.removeEntity = function(entity) {
+	// Find the entity in the list of entities if it is present
+    for (var i = 0; i < this._entities.length; i++) {
+        if (this._entities[i] == entity) {
+            this._entities.splice(i, 1);
+            break;
+        }
+    }
+    // If the entity is an actor, remove them from the scheduler
+    if (entity.hasMixin('Actor')) {
+        this._scheduler.remove(entity);
+    }
+};
 Game.Map.prototype.addEntityAtRandomPosition = function(entity) {
 	var position = this.getRandomFloorPosition();
 	entity.setX(position.x);
@@ -81,12 +94,15 @@ Game.Map.prototype.dig = function(x, y) {
 		this._tiles[x][y] = Game.Tile.floorTile;
 	}
 };
-
+Game.Map.prototype.isEmptyFloor = function(x, y) {
+    // Check if the tile is floor and also has no entity
+    return this.getTile(x, y) == Game.Tile.floorTile && !this.getEntityAt(x, y);
+};
 Game.Map.prototype.getRandomFloorPosition = function() {
 	var x, y;
 	do {
 		x = Math.floor(Math.random() * this._width);
 		y = Math.floor(Math.random() * this._height);
-	} while(this.getTile(x, y) != Game.Tile.floorTile || this.getEntityAt(x, y));
+	} while(!this.isEmptyFloor(x, y));
 	return {x: x, y: y};
 };
