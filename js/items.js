b19ee2486ae1f851730d145317ce1136e4198314
Jake Franklin <jacob.d.franklin@gmail.com>
2017-02-07 16:22:51 -0700

    A couple new items

--- js/items.js
+++ js/items.js
@@ -102,6 +102,20 @@ Game.ItemRepository.define('sink', {
     mixins: [Game.ItemMixins.Fixture]
 });
 
+Game.ItemRepository.define('dresser', {
+    name: 'dresser',
+    character: ']',
+    foreground: Game.Palette.brown,
+    mixins: [Game.ItemMixins.Fixture, Game.ItemMixins.Container]
+});
+
+Game.ItemRepository.define('bookshelf', {
+    name: 'bookshelf',
+    character: '[',
+    foreground: Game.Palette.brown,
+    mixins: [Game.ItemMixins.Fixture, Game.ItemMixins.Container]
+});
+
 Game.ItemRepository.define('corpse', {
     name: 'corpse',
     character: '%',
@@ -112,5 +126,5 @@ Game.ItemRepository.define('sign', {
     name: 'sign',
     character: '?',
     foreground: Game.Palette.white,
-    mixins: [Game.ItemMixins.Inscribable]
+    mixins: [Game.ItemMixins.Inscribable, Game.ItemMixins.Fixture]
 });
