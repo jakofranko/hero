d26056fd221314dd725eace140f71aa5f1601114
Jake Franklin <jacob.d.franklin@gmail.com>
2017-05-09 16:17:42 -0700

    Updates to the event-source prototype

--- js/event-source.js
+++ js/event-source.js
@@ -1,6 +1,6 @@
 // An event source is essentially an actor that, every turn, has a low chance of 'spawning' an event. Event sources determine the kinds of events they can spawn, how often, and logic on how to disable them (if applicable) 
 Game.EventSource = function(properties) {
-    var requiredProps = ['name', 'maxEvents', 'eventTypes', 'spawnChance'];
+    var requiredProps = ['name', 'maxEvents', 'eventTypes'];
 
     // Make sure the required properties are defined
     requiredProps.forEach(p => {
@@ -8,24 +8,43 @@ Game.EventSource = function(properties) {
             throw new Error(`Event source must have ${p} defined`);
     });
 
-    this.name               = properties['name'];
-    this.maxActiveEvents    = properties['maxActiveEvents'];     // 2; Max
-    this.activeEvents       = [];
-    this.eventTypes         = properties['eventTypes'];    // ['robbery', 'drug deal', 'arms deal']
-    this.spawnChance        = properties['spawnChance'];   // 0.01
+    this._map                = null;
+    this._name               = properties['name'];
+    this._maxActiveEvents    = properties['maxActiveEvents'];     // 2; Max
+    this._activeEvents       = [];
+    this._eventTypes         = properties['eventTypes'];    // ['robbery', 'drug deal', 'arms deal']
+    this._spawnChance        = properties['spawnChance'] || 0.01;   // Between 0 and 1, 0 being no chance and 1 being every time
 
+    // TODO: need to add property 'spawnCondition' that is factored when 
+    // spawning to handle things like "only spawn if spawnChance && crime > 50" etc.
+
+    // Should only act once per 'round'
+    this._speed = 1;
+};
+Game.EventSource.prototype.getName = function() {
+    return this._name;
+};
+Game.EventSource.prototype.getMap = function() {
+    return this._map;
+};
+Game.EventSource.prototype.getActiveEvents = function() {
+    return this._map;
 };
 
-Game.EventSource.prototype.spawn = function() {
+Game.EventSource.prototype.act = function() {
+    if(Math.random() < this._spawnChance)
+        this._spawn();
+};
+
+Game.EventSource.prototype._spawn = function() {
     if(this.activeEvents.length + 1 > this.maxActiveEvents)
         return false;
 
     var type = this.eventTypes.random(),
-        event = new Game.Event(type);
+        event = Game.EventRepository.create(type, {map: this._map});
 
     event.start();
-    this.activeEvents.push(event);+
+    this.activeEvents.push(event);
 
-    Game.addActiveEvent(event);
     return true;
 };
