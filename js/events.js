1a265b1e94bf1dac4bb00ec3b67d65790f9111b2
Jake Franklin <jacob.d.franklin@gmail.com>
2017-05-09 16:18:43 -0700

    Turned Game.Events into a repository Game.EventRepository

--- js/events.js
+++ js/events.js
@@ -1,16 +1,30 @@
-// Events will have the logic for the entities involved in it, where it will take place, and the logic for how the event 'succeeds' and 'fails.' Entities that are spawned as part of an event should have a special mixin that has listeners for events like onDeath, which will in turn trigger events for the 'active events'
+// Events will have the logic for the entities involved in it, where it will take place, 
+// and the logic for how the event 'succeeds' and 'fails.' Entities that are spawned as 
+// part of an event should have a special mixin that has listeners for events like onDeath, 
+// which will in turn trigger events for the 'active events'.
+// var event = {
+//     name: 'robbery',
+//     spawnLocations: ['cash register', 'vault', 'jewlery case'],
+//     entities: ['gunman', 'robber'],
+//     minEntities: 2,
+//     maxEntities: 6,
+//     numEntities: null,
+//     onEntityDeath: function() {
+//         if(numEntities < 1)
+//             Game.Events.eventSuccess(this);
+//         else
+//             numEntities--;
+//     }
+// };
+Game.EventRepository = new Game.Repository('events', Game.Event);
 
-var event = {
-    name: 'robbery',
-    spawnLocations: ['cash register', 'vault', 'jewlery case'],
-    entities: ['gunman', 'robber'],
+// Note: the 'map' property is required for all events, but will need to 
+// be specified by the event-source (which, since it is functioning as 
+// an actor will have the map associated with it)
+Game.EventRepository.define('bank robbery', {
+    name: 'bank robbery',
+    spawnLocations: ['vault door', 'cash register', 'safe'],
+    entityTypes: ['person'], // TODO: Create specific templates for 'robbers', 'gunman' etc. 
     minEntities: 2,
-    maxEntities: 6,
-    numEntities: null,
-    onEntityDeath: function() {
-        if(numEntities < 1)
-            Game.Events.eventSuccess(this);
-        else
-            numEntities--;
-    }
-};
+    maxEntities: 6
+});
