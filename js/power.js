3570566a0775f45a96d702f22c2f3ec75b71dee4
Jake Franklin <jacob.d.franklin@gmail.com>
2017-07-06 15:59:58 -0600

    Beginning powers...

---
+++ js/power.js
@@ -0,0 +1,13 @@
+// TODO: [POWERS] Support for advantages
+// TODO: [POWERS] Support for adders
+// TODO: [POWERS] Support for limitations
+// TODO: [POWERS] Support for 'compound powers'
+// TODO: [POWERS] Support for power frameworks
+Game.Power =  function(properties) {
+    name
+    type
+    cost
+    range
+    duration
+    
+};
