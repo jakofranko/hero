ef9ccea23f96f3e1d58a1f2f2163662b2bb77ced
Jake Franklin <jacob.d.franklin@gmail.com>
2017-08-14 07:15:12 -0700

    Continuing to flesh out the power prototype

--- js/power.js
+++ js/power.js
@@ -29,6 +29,10 @@ Game.Power =  function(properties) {
         throw new Error(`The range '${properties['range']}' is not valid, and must be one of these: ${RANGES.join(', ')}`);
     if(!properties[properties['duration']])
         throw new Error(`You must define a '${properties['duration']}' function in the properties`);
+    if(properties['duration'] != 'instant' && (properties['onQueue'] === undefined && properties['onDequeue'] === undefined))
+        throw new Error(`The power ${properties.name} has a duration of '${properties.duration}' and must have both an 'onQueue' and 'onDequeue' function defined`);
+    if(properties['type'] === 'attack' && properties['damageType'] === undefined)
+        throw new Error(`You must define a damage type for attack power ${properties.name}`);
 
     this.name      = properties['name'];
     this.type      = properties['type'];
@@ -39,29 +43,30 @@ Game.Power =  function(properties) {
     this.points    = properties['points'] || 0;
     this.entity    = properties['entity'];
     this.END       = properties['END'] || function() { return Math.max(1, Math.round(points / 10)); };
+    this.damageType = properties['damageType'];
 
     // Depending on the type of range, assign a different function to the range property
     switch(properties['range']) {
         case 'self':
-            this.range = function(targetX, targetY) {
+            this.inRange = function(startX, startY, targetX, targetY) {
                 return this.entity.getMap().getEntityAt(targetX, targetY, this.entity.getZ()) === this.entity;
-            }
+            };
             break;
         case 'no range':
-            this.range = function(targetX, targetY) {
+            this.inRange = function(startX, startY, targetX, targetY) {
                 return Game.Geometry.distance(this.entity.getX(), this.entity.getY(), targetX, targetY) <= 1;
             };
             break;
         case 'standard':
-            this.range = function(startX, startY, targetX, targetY) {
+            this.inRange = function(startX, startY, targetX, targetY) {
                 return Game.Geometry.distance(this.entity.getX(), this.entity.getY(), targetX, targetY) <= (5 * this.points);
-            }
+            };
             break;
         case 'LOS':
-            this.range = function(targetX, targetY) {
+            this.inRange = function(startX, startY, targetX, targetY) {
                 let target = this.entity.getMap().getEntityAt(targetX, targetY, this.entity.getZ());
                 return target && this.entity.hasMixin('Sight') && this.entity.canSee(target);
-            }
+            };
             break;
     }
 
@@ -71,5 +76,8 @@ Game.Power =  function(properties) {
     // character's END, and do it immediately. If the power is activating a constant power, then the 
     // effect should be to spend the entities endurance, and then add the power to the character's list
     // of active powers.
-    this.effect    = properties['effect'] || function() { console.error("This power needs to have an effect function defined."); };
+    this.effect = properties['effect'] || function() { console.error("This power needs to have an effect function defined."); };
+
+    this.onQueue = properties['onQueue'] || function() {};
+    this.onDequeue = properties['onDequeue'] || function() {};
 };
