c7fee528a984707927c168d9b9b9275631e9c507
Jake Franklin <jacob.d.franklin@gmail.com>
2018-01-10 20:14:54 -0700

    Removed old mixin from different game

--- js/entity-mixins.js
+++ js/entity-mixins.js
@@ -578,39 +578,6 @@ Game.EntityMixins.FoodConsumer = {
         }
     }
 };
-Game.EntityMixins.FungusActor = {
-    name: 'FungusActor',
-    groupName: 'Actor',
-    init: function() {
-        this._growthsRemaining = 5;
-    },
-    act: function() {
-        if(this._growthsRemaining > 0) {
-            if(Math.random() <= 0.02) {
-                // Generate the coordinates of a random adjacent square by
-                // generating an offset between [-1, 0, 1] for both the x and
-                // y directions. To do this, we generate a number from 0-2 and then
-                // subtract 1.
-                var xOffset = Math.floor(Math.random() * 3) - 1;
-                var yOffset = Math.floor(Math.random() * 3) - 1;
-                // Make sure we aren't trying to spawn on the same tile as us
-                if (xOffset != 0 || yOffset != 0) {
-                    // Check if we can actually spawn at that location, and if so
-                    // then we grow!
-                    if (this.getMap().isEmptyFloor(this.getX() + xOffset, this.getY() + yOffset, this.getZ())) {
-                        var entity = Game.EntityRepository.create('fungus');
-                        entity.setPosition(this.getX() + xOffset, this.getY() + yOffset, this.getZ());
-                        this.getMap().addEntity(entity);
-                        this._growthsRemaining--;
-
-                        // Send a message nearby!
-                        Game.sendMessageNearby(this.getMap(), entity.getX(), entity.getY(), entity.getZ(), 'The fungus is spreading!');
-                    }
-                }
-            }
-        }
-    }
-};
 Game.EntityMixins.InventoryHolder = {
     name: 'InventoryHolder',
     init: function(template) {
