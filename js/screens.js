aada83656e6c6830e048b1c5d30bbacbc82b7b10
Jake Franklin <jacob.d.franklin@gmail.com>
2018-12-31 12:04:39 -0700

    Version 1.0

--- js/screens.js
+++ js/screens.js
@@ -88,7 +88,7 @@ Game.Screen.startScreen = {
         //                     __,,aaaadd888888888888888bbaaaa,,__
         //                   d8888888888888888888888888888888888888b
 
-        var version = "v0.6";
+        var version = "v1.0";
         display.drawText((w / 2) - (version.length / 2), scalesASCII.length + y, version);
 
     },
